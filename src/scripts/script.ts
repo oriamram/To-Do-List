fcc1f8b122aa959d8b975a609784cc053044a67c
student <oriamram@cyber4s.dev>
2022-05-28 17:53:29 +0300

    added local storage

--- src/scripts/script.ts
+++ src/scripts/script.ts
@@ -1,10 +1,38 @@
 const noteBook = document.getElementById('noteBook') as HTMLElement;
-let rowCounter: number = 1;
 let editClicked: boolean = false;
 let editor: HTMLInputElement;
+if (localStorage.getItem('ids') === null) {
+    localStorage.setItem('ids', '1');
+}
+for (let task in localStorage) {
+    if (!isNaN(+task)) {
+        const checkTask = document.createElement('div');
+        checkTask.classList.add('task');
+        checkTask.setAttribute('id', '' + task);
+        noteBook.appendChild(checkTask);
+        const check = document.createElement('input');
+        check.setAttribute('type', 'checkBox');
+        check.classList.add('checkBox');
+        check.addEventListener('click', () => isChecked(checkTask));
+        checkTask.appendChild(check);
+        const line = document.createElement('h3');
+        line.innerText = '' + localStorage.getItem(task);
+        checkTask.appendChild(line);
+        createEditButton(checkTask).addEventListener('click', () => onEdit(checkTask));
+        createDltButton(checkTask).addEventListener('click', () => onDlt(checkTask));
+    }
+    if(localStorage.length<=12){
+        
+    }
+}
+
 let input: HTMLInputElement = createInput();
 let enter: HTMLElement = createButton();
 enter.addEventListener('click', () => addTask(input, enter));
+if(localStorage.length===13){
+    input.remove();
+    enter.remove();
+}
 
 //creates the add btn
 function createButton(): HTMLElement {
@@ -44,12 +72,14 @@ function createDltButton(task: HTMLElement): HTMLElement {
 function onDlt(task: HTMLElement) {
     if (!editClicked) {
         task.remove();
-        rowCounter--;
-    }
-    if(rowCounter<=12&&noteBook.querySelector('#taskBox')===null){
-        input=createInput();
-        enter = createButton();
-        enter.addEventListener('click', () => addTask(input, enter));
+        localStorage.removeItem(task.id);
+        console.log(localStorage);
+        if (localStorage.length <= 11 && noteBook.querySelector('#taskBox') === null) {
+
+            input = createInput();
+            enter = createButton();
+            enter.addEventListener('click', () => addTask(input, enter));
+        }
     }
 }
 //creates the edit btn
@@ -68,6 +98,7 @@ function onEdit(task: HTMLElement) {
         if (typeof task.children[1].innerHTML === 'string') {
             text = task.children[1].innerHTML;
         }
+
         input.remove();
         enter.remove();
         task.children[1].innerHTML = '';
@@ -77,29 +108,35 @@ function onEdit(task: HTMLElement) {
 
     } else if (editClicked && task.children[1].innerHTML.length === 0) {
         if (!outOfLine(editor)) {
+            const id: string = '' + task.getAttribute('id');
+            localStorage.removeItem(id);
+            localStorage.setItem(id, editor.value);
             editor.remove();
             editClicked = false;
             task.children[1].innerHTML = editor.value;
-            input = createInput();
-            enter = createButton();
-            enter.addEventListener('click', () => addTask(input, enter));
+            if (localStorage.length < 11) {
+                input = createInput();
+                enter = createButton();
+                enter.addEventListener('click', () => addTask(input, enter));
+            }
         }
     }
 
 }
-function isChecked(task:HTMLElement){
+function isChecked(task: HTMLElement) {
     task.classList.toggle('checked');
 }
-//what happens when press on enter
+//when press on enter
 function addTask(field: HTMLInputElement, button: HTMLElement) {
-    if (!outOfLine(field) && rowCounter <= 12) {
+    if (!outOfLine(field) && localStorage.length <= 13) {
         const checkTask = document.createElement('div');
         checkTask.classList.add('task');
+        checkTask.setAttribute('id', '' + localStorage.getItem('ids'));
         noteBook.appendChild(checkTask);
         const check = document.createElement('input');
         check.setAttribute('type', 'checkBox');
         check.classList.add('checkBox');
-        check.addEventListener('click',()=>isChecked(checkTask));
+        check.addEventListener('click', () => isChecked(checkTask));
         checkTask.appendChild(check);
         const line = document.createElement('h3');
         line.innerText = field.value;
@@ -107,20 +144,18 @@ function addTask(field: HTMLInputElement, button: HTMLElement) {
         input.remove();
         button.remove();
         createEditButton(checkTask).addEventListener('click', () => onEdit(checkTask));
-        createDltButton(checkTask).addEventListener('click', () => onDlt(checkTask));
         input = createInput();
         enter = createButton();
         enter.addEventListener('click', () => addTask(input, enter));
-        if (rowCounter === 12) {
+        localStorage.setItem((localStorage.ids).toString(), field.value.toString());
+        localStorage.ids = (+('' + localStorage.getItem('ids')) + 1).toString();
+        createDltButton(checkTask).addEventListener('click', () => onDlt(checkTask));
+        if (localStorage.length === 13) {
             input.remove();
-            enter.remove(); 
+            enter.remove();
         }
-        rowCounter++;
     }
-    else if (rowCounter >= 13) {
-        alert("yes yes keep diggin' \ni already thought about it");
-        field.value = '';
-    }
+    location.reload();
 }
 //check for a valied input
 function outOfLine(input: HTMLInputElement): boolean {
